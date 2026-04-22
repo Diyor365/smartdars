61a00c40b26ec6a9cafd75d7a4e1b937ad815ef3
diyorbek6875 <diyorbekabdusalomov32@gmail.com>
2026-04-22 15:11:18 +0500

    add config

---
+++ config.py
@@ -0,0 +1,18 @@
+# Config file
+# Bot tokeningizni qo'ying!
+
+TOKEN = "8683373688:AAFre5uruXzwlQaedOngDMPVsI2CjVzfECU"
+
+# Database nomi
+DATABASE_NAME = "smartdars.db"
+
+# Bot admin telegram ID (ixtiyoriy)
+# Admin bo'lish uchun bizga adminningiz Telegram ID si kerak!
+# Bot: @userinfobot dan ID'ni oling
+ADMIN_ID = 1937466761# Misol: ADMIN_ID = 123456789
+
+# Test davomiyligi (soatlar)
+TEST_TIMEOUT = 10
+
+# Admin qo'shimcha tayfaddalar (birgalikda multiple admins)
+ADMIN_IDS = [1336583023,1937466761]  # Misol: [123456789, 987654321]
